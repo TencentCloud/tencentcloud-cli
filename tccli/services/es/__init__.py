e7c1785ed0f557d064a1994e8a2d3722af26500f
tencentcloudapi <tencentcloudapi@tencent.com>
2018-05-31 22:40:08 +0800

    release 3.0.9.1

--- tccli/services/es/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-from tccli.services.es.es_client import register_arg
-from tccli.services.es.es_client import get_actions_info
-from tccli.services.es.es_client import AVAILABLE_VERSION_LIST
