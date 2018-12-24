4c95d9bd362f69bdf515fa82295da0b8039ec532
tencentcloudapi <tencentcloudapi@tencent.com>
2018-12-24 20:16:01 +0800

    release 3.0.47.1

---
+++ tccli/services/tke/tke_client.py
@@ -0,0 +1,326 @@
+# -*- coding: utf-8 -*-
+import os
+import json
+import tccli.options_define as OptionsDefine
+import tccli.format_output as FormatOutput
+from tccli.nice_command import NiceCommand
+import tccli.error_msg as ErrorMsg
+import tccli.help_template as HelpTemplate
+from tccli import __version__
+from tccli.utils import Utils
+from tccli.configure import Configure
+from tencentcloud.common import credential
+from tencentcloud.common.profile.http_profile import HttpProfile
+from tencentcloud.common.profile.client_profile import ClientProfile
+from tencentcloud.tke.v20180525 import tke_client as tke_client_v20180525
+from tencentcloud.tke.v20180525 import models as models_v20180525
+from tccli.services.tke import v20180525
+from tccli.services.tke.v20180525 import help as v20180525_help
+
+
+def doAddExistedInstances(argv, arglist):
+    g_param = parse_global_arg(argv)
+    if "help" in argv:
+        show_help("AddExistedInstances", g_param[OptionsDefine.Version])
+        return
+
+    param = {
+        "ClusterId": Utils.try_to_json(argv, "--ClusterId"),
+        "InstanceIds": Utils.try_to_json(argv, "--InstanceIds"),
+        "InstanceAdvancedSettings": Utils.try_to_json(argv, "--InstanceAdvancedSettings"),
+        "EnhancedService": Utils.try_to_json(argv, "--EnhancedService"),
+        "LoginSettings": Utils.try_to_json(argv, "--LoginSettings"),
+        "SecurityGroupIds": Utils.try_to_json(argv, "--SecurityGroupIds"),
+
+    }
+    cred = credential.Credential(g_param[OptionsDefine.SecretId], g_param[OptionsDefine.SecretKey])
+    http_profile = HttpProfile(
+        reqTimeout=60 if g_param[OptionsDefine.Timeout] is None else int(g_param[OptionsDefine.Timeout]),
+        reqMethod="POST",
+        endpoint=g_param[OptionsDefine.Endpoint]
+    )
+    profile = ClientProfile(httpProfile=http_profile, signMethod="HmacSHA256")
+    mod = CLIENT_MAP[g_param[OptionsDefine.Version]]
+    client = mod.TkeClient(cred, g_param[OptionsDefine.Region], profile)
+    client._sdkVersion += ("_CLI_" + __version__)
+    models = MODELS_MAP[g_param[OptionsDefine.Version]]
+    model = models.AddExistedInstancesRequest()
+    model.from_json_string(json.dumps(param))
+    rsp = client.AddExistedInstances(model)
+    result = rsp.to_json_string()
+    jsonobj = None
+    try:
+        jsonobj = json.loads(result)
+    except TypeError as e:
+        jsonobj = json.loads(result.decode('utf-8')) # python3.3
+    FormatOutput.output("action", jsonobj, g_param[OptionsDefine.Output], g_param[OptionsDefine.Filter])
+
+
+def doDescribeClusters(argv, arglist):
+    g_param = parse_global_arg(argv)
+    if "help" in argv:
+        show_help("DescribeClusters", g_param[OptionsDefine.Version])
+        return
+
+    param = {
+        "ClusterIds": Utils.try_to_json(argv, "--ClusterIds"),
+        "Offset": Utils.try_to_json(argv, "--Offset"),
+        "Limit": Utils.try_to_json(argv, "--Limit"),
+
+    }
+    cred = credential.Credential(g_param[OptionsDefine.SecretId], g_param[OptionsDefine.SecretKey])
+    http_profile = HttpProfile(
+        reqTimeout=60 if g_param[OptionsDefine.Timeout] is None else int(g_param[OptionsDefine.Timeout]),
+        reqMethod="POST",
+        endpoint=g_param[OptionsDefine.Endpoint]
+    )
+    profile = ClientProfile(httpProfile=http_profile, signMethod="HmacSHA256")
+    mod = CLIENT_MAP[g_param[OptionsDefine.Version]]
+    client = mod.TkeClient(cred, g_param[OptionsDefine.Region], profile)
+    client._sdkVersion += ("_CLI_" + __version__)
+    models = MODELS_MAP[g_param[OptionsDefine.Version]]
+    model = models.DescribeClustersRequest()
+    model.from_json_string(json.dumps(param))
+    rsp = client.DescribeClusters(model)
+    result = rsp.to_json_string()
+    jsonobj = None
+    try:
+        jsonobj = json.loads(result)
+    except TypeError as e:
+        jsonobj = json.loads(result.decode('utf-8')) # python3.3
+    FormatOutput.output("action", jsonobj, g_param[OptionsDefine.Output], g_param[OptionsDefine.Filter])
+
+
+def doDeleteClusterInstances(argv, arglist):
+    g_param = parse_global_arg(argv)
+    if "help" in argv:
+        show_help("DeleteClusterInstances", g_param[OptionsDefine.Version])
+        return
+
+    param = {
+        "ClusterId": Utils.try_to_json(argv, "--ClusterId"),
+        "InstanceIds": Utils.try_to_json(argv, "--InstanceIds"),
+        "InstanceDeleteMode": Utils.try_to_json(argv, "--InstanceDeleteMode"),
+
+    }
+    cred = credential.Credential(g_param[OptionsDefine.SecretId], g_param[OptionsDefine.SecretKey])
+    http_profile = HttpProfile(
+        reqTimeout=60 if g_param[OptionsDefine.Timeout] is None else int(g_param[OptionsDefine.Timeout]),
+        reqMethod="POST",
+        endpoint=g_param[OptionsDefine.Endpoint]
+    )
+    profile = ClientProfile(httpProfile=http_profile, signMethod="HmacSHA256")
+    mod = CLIENT_MAP[g_param[OptionsDefine.Version]]
+    client = mod.TkeClient(cred, g_param[OptionsDefine.Region], profile)
+    client._sdkVersion += ("_CLI_" + __version__)
+    models = MODELS_MAP[g_param[OptionsDefine.Version]]
+    model = models.DeleteClusterInstancesRequest()
+    model.from_json_string(json.dumps(param))
+    rsp = client.DeleteClusterInstances(model)
+    result = rsp.to_json_string()
+    jsonobj = None
+    try:
+        jsonobj = json.loads(result)
+    except TypeError as e:
+        jsonobj = json.loads(result.decode('utf-8')) # python3.3
+    FormatOutput.output("action", jsonobj, g_param[OptionsDefine.Output], g_param[OptionsDefine.Filter])
+
+
+def doDescribeClusterInstances(argv, arglist):
+    g_param = parse_global_arg(argv)
+    if "help" in argv:
+        show_help("DescribeClusterInstances", g_param[OptionsDefine.Version])
+        return
+
+    param = {
+        "ClusterId": Utils.try_to_json(argv, "--ClusterId"),
+        "Offset": Utils.try_to_json(argv, "--Offset"),
+        "Limit": Utils.try_to_json(argv, "--Limit"),
+        "InstanceIds": Utils.try_to_json(argv, "--InstanceIds"),
+
+    }
+    cred = credential.Credential(g_param[OptionsDefine.SecretId], g_param[OptionsDefine.SecretKey])
+    http_profile = HttpProfile(
+        reqTimeout=60 if g_param[OptionsDefine.Timeout] is None else int(g_param[OptionsDefine.Timeout]),
+        reqMethod="POST",
+        endpoint=g_param[OptionsDefine.Endpoint]
+    )
+    profile = ClientProfile(httpProfile=http_profile, signMethod="HmacSHA256")
+    mod = CLIENT_MAP[g_param[OptionsDefine.Version]]
+    client = mod.TkeClient(cred, g_param[OptionsDefine.Region], profile)
+    client._sdkVersion += ("_CLI_" + __version__)
+    models = MODELS_MAP[g_param[OptionsDefine.Version]]
+    model = models.DescribeClusterInstancesRequest()
+    model.from_json_string(json.dumps(param))
+    rsp = client.DescribeClusterInstances(model)
+    result = rsp.to_json_string()
+    jsonobj = None
+    try:
+        jsonobj = json.loads(result)
+    except TypeError as e:
+        jsonobj = json.loads(result.decode('utf-8')) # python3.3
+    FormatOutput.output("action", jsonobj, g_param[OptionsDefine.Output], g_param[OptionsDefine.Filter])
+
+
+CLIENT_MAP = {
+    "v20180525": tke_client_v20180525,
+
+}
+
+MODELS_MAP = {
+    "v20180525": models_v20180525,
+
+}
+
+ACTION_MAP = {
+    "AddExistedInstances": doAddExistedInstances,
+    "DescribeClusters": doDescribeClusters,
+    "DeleteClusterInstances": doDeleteClusterInstances,
+    "DescribeClusterInstances": doDescribeClusterInstances,
+
+}
+
+AVAILABLE_VERSION_LIST = [
+    v20180525.version,
+
+]
+AVAILABLE_VERSIONS = {
+     'v' + v20180525.version.replace('-', ''): {"help": v20180525_help.INFO,"desc": v20180525_help.DESC},
+
+}
+
+
+def tke_action(argv, arglist):
+    if "help" in argv:
+        versions = sorted(AVAILABLE_VERSIONS.keys())
+        opt_v = "--" + OptionsDefine.Version
+        version = versions[-1]
+        if opt_v in argv:
+            version = 'v' + argv[opt_v].replace('-', '')
+        if version not in versions:
+            print("available versions: %s" % " ".join(AVAILABLE_VERSION_LIST))
+            return
+        action_str = ""
+        docs = AVAILABLE_VERSIONS[version]["help"]
+        desc = AVAILABLE_VERSIONS[version]["desc"]
+        for action, info in docs.items():
+            action_str += "        %s\n" % action
+            action_str += Utils.split_str("        ", info["desc"], 120)
+        helpstr = HelpTemplate.SERVICE % {"name": "tke", "desc": desc, "actions": action_str}
+        print(helpstr)
+    else:
+        print(ErrorMsg.FEW_ARG)
+
+
+def version_merge():
+    help_merge = {}
+    for v in AVAILABLE_VERSIONS:
+        for action in AVAILABLE_VERSIONS[v]["help"]:
+            if action not in help_merge:
+                help_merge[action] = {}
+            help_merge[action]["cb"] = ACTION_MAP[action]
+            help_merge[action]["params"] = []
+            for param in AVAILABLE_VERSIONS[v]["help"][action]["params"]:
+                if param["name"] not in help_merge[action]["params"]:
+                    help_merge[action]["params"].append(param["name"])
+    return help_merge
+
+
+def register_arg(command):
+    cmd = NiceCommand("tke", tke_action)
+    command.reg_cmd(cmd)
+    cmd.reg_opt("help", "bool")
+    cmd.reg_opt(OptionsDefine.Version, "string")
+    help_merge = version_merge()
+
+    for actionName, action in help_merge.items():
+        c = NiceCommand(actionName, action["cb"])
+        cmd.reg_cmd(c)
+        c.reg_opt("help", "bool")
+        for param in action["params"]:
+            c.reg_opt("--" + param, "string")
+
+        for opt in OptionsDefine.ACTION_GLOBAL_OPT:
+            stropt = "--" + opt
+            c.reg_opt(stropt, "string")
+
+
+def parse_global_arg(argv):
+    params = {}
+    for opt in OptionsDefine.ACTION_GLOBAL_OPT:
+        stropt = "--" + opt
+        if stropt in argv:
+            params[opt] = argv[stropt]
+        else:
+            params[opt] = None
+    if params[OptionsDefine.Version]:
+        params[OptionsDefine.Version] = "v" + params[OptionsDefine.Version].replace('-', '')
+
+    config_handle = Configure()
+    profile = config_handle.profile
+    if ("--" + OptionsDefine.Profile) in argv:
+        profile = argv[("--" + OptionsDefine.Profile)]
+
+    is_conexist, conf_path = config_handle._profile_existed(profile + "." + config_handle.configure)
+    is_creexist, cred_path = config_handle._profile_existed(profile + "." + config_handle.credential)
+    config = {}
+    cred = {}
+    if is_conexist:
+        config = config_handle._load_json_msg(conf_path)
+    if is_creexist:
+        cred = config_handle._load_json_msg(cred_path)
+
+    for param in params.keys():
+        if param == OptionsDefine.Version:
+            continue
+        if params[param] is None:
+            if param in [OptionsDefine.SecretKey, OptionsDefine.SecretId]:
+                if param in cred:
+                    params[param] = cred[param]
+                else:
+                    raise Exception("%s is invalid" % param)
+            else:
+                if param in config:
+                    params[param] = config[param]
+                elif param == OptionsDefine.Region:
+                    raise Exception("%s is invalid" % OptionsDefine.Region)
+    try:
+        if params[OptionsDefine.Version] is None:
+            version = config["tke"][OptionsDefine.Version]
+            params[OptionsDefine.Version] = "v" + version.replace('-', '')
+
+        if params[OptionsDefine.Endpoint] is None:
+            params[OptionsDefine.Endpoint] = config["tke"][OptionsDefine.Endpoint]
+    except Exception as err:
+        raise Exception("config file:%s error, %s" % (conf_path, str(err)))
+    versions = sorted(AVAILABLE_VERSIONS.keys())
+    if params[OptionsDefine.Version] not in versions:
+        raise Exception("available versions: %s" % " ".join(AVAILABLE_VERSION_LIST))
+    return params
+
+
+def show_help(action, version):
+    docs = AVAILABLE_VERSIONS[version]["help"][action]
+    desc = AVAILABLE_VERSIONS[version]["desc"]
+    docstr = ""
+    for param in docs["params"]:
+        docstr += "        %s\n" % ("--" + param["name"])
+        docstr += Utils.split_str("        ", param["desc"], 120)
+
+    helpmsg = HelpTemplate.ACTION % {"name": action, "service": "tke", "desc": desc, "params": docstr}
+    print(helpmsg)
+
+
+def get_actions_info():
+    config = Configure()
+    new_version = max(AVAILABLE_VERSIONS.keys())
+    version = new_version
+    try:
+        profile = config._load_json_msg(os.path.join(config.cli_path, "default.configure"))
+        version = profile["tke"]["version"]
+        version = "v" + version.replace('-', '')
+    except Exception:
+        pass
+    if version not in AVAILABLE_VERSIONS.keys():
+        version = new_version
+    return AVAILABLE_VERSIONS[version]["help"]
---
+++ tccli/services/tke/v20180525/help.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+DESC = "tke-2018-05-25"
+INFO = {
+  "AddExistedInstances": {
+    "params": [
+      {
+        "name": "ClusterId",
+        "desc": "集群ID"
+      },
+      {
+        "name": "InstanceIds",
+        "desc": "实例列表"
+      },
+      {
+        "name": "InstanceAdvancedSettings",
+        "desc": "实例额外需要设置参数信息"
+      },
+      {
+        "name": "EnhancedService",
+        "desc": "增强服务。通过该参数可以指定是否开启云安全、云监控等服务。若不指定该参数，则默认开启云监控、云安全服务。"
+      },
+      {
+        "name": "LoginSettings",
+        "desc": "节点登录信息（目前仅支持使用Password或者单个KeyIds）"
+      },
+      {
+        "name": "SecurityGroupIds",
+        "desc": "实例所属安全组。该参数可以通过调用 DescribeSecurityGroups 的返回值中的sgId字段来获取。若不指定该参数，则绑定默认安全组。（目前仅支持设置单个sgId）"
+      }
+    ],
+    "desc": "添加已经存在的实例到集群"
+  },
+  "DescribeClusters": {
+    "params": [
+      {
+        "name": "ClusterIds",
+        "desc": "集群ID列表(为空时，\n表示获取账号下所有集群)"
+      },
+      {
+        "name": "Offset",
+        "desc": "偏移量,默认0"
+      },
+      {
+        "name": "Limit",
+        "desc": "最大输出条数，默认20"
+      }
+    ],
+    "desc": "查询集群列表"
+  },
+  "DeleteClusterInstances": {
+    "params": [
+      {
+        "name": "ClusterId",
+        "desc": "集群ID"
+      },
+      {
+        "name": "InstanceIds",
+        "desc": "主机InstanceId列表"
+      },
+      {
+        "name": "InstanceDeleteMode",
+        "desc": "集群实例删除时的策略：terminate（销毁实例，仅支持按量计费云主机实例） retain （仅移除，保留实例）"
+      }
+    ],
+    "desc": "删除集群中的实例"
+  },
+  "DescribeClusterInstances": {
+    "params": [
+      {
+        "name": "ClusterId",
+        "desc": "集群ID"
+      },
+      {
+        "name": "Offset",
+        "desc": "偏移量,默认0"
+      },
+      {
+        "name": "Limit",
+        "desc": "最大输出条数，默认20"
+      },
+      {
+        "name": "InstanceIds",
+        "desc": "需要获取的节点实例Id列表(默认为空，表示拉取集群下所有节点实例)"
+      }
+    ],
+    "desc": " 查询集群下节点实例信息 "
+  }
+}
