301a9985be1d95b8fecc574e4ed8db9e385049bc
tencentcloudapi <tencentcloudapi@tenent.com>
2020-09-18 09:10:19 +0800

    release 3.0.259.1

--- tccli/services/captcha/captcha_client.py
+++ tccli/services/captcha/captcha_client.py
@@ -213,6 +213,31 @@ def doDescribeCaptchaMiniDataSum(args, parsed_globals):
     FormatOutput.output("action", jsonobj, g_param[OptionsDefine.Output], g_param[OptionsDefine.Filter])
 
 
+def doDescribeCaptchaMiniResult(args, parsed_globals):
+    g_param = parse_global_arg(parsed_globals)
+
+    cred = credential.Credential(g_param[OptionsDefine.SecretId], g_param[OptionsDefine.SecretKey])
+    http_profile = HttpProfile(
+        reqTimeout=60 if g_param[OptionsDefine.Timeout] is None else int(g_param[OptionsDefine.Timeout]),
+        reqMethod="POST",
+        endpoint=g_param[OptionsDefine.Endpoint]
+    )
+    profile = ClientProfile(httpProfile=http_profile, signMethod="HmacSHA256")
+    mod = CLIENT_MAP[g_param[OptionsDefine.Version]]
+    client = mod.CaptchaClient(cred, g_param[OptionsDefine.Region], profile)
+    client._sdkVersion += ("_CLI_" + __version__)
+    models = MODELS_MAP[g_param[OptionsDefine.Version]]
+    model = models.DescribeCaptchaMiniResultRequest()
+    model.from_json_string(json.dumps(args))
+    rsp = client.DescribeCaptchaMiniResult(model)
+    result = rsp.to_json_string()
+    try:
+        jsonobj = json.loads(result)
+    except TypeError as e:
+        jsonobj = json.loads(result.decode('utf-8'))  # python3.3
+    FormatOutput.output("action", jsonobj, g_param[OptionsDefine.Output], g_param[OptionsDefine.Filter])
+
+
 def doUpdateCaptchaAppIdInfo(args, parsed_globals):
     g_param = parse_global_arg(parsed_globals)
 
@@ -307,6 +332,7 @@ ACTION_MAP = {
     "DescribeCaptchaMiniData": doDescribeCaptchaMiniData,
     "DescribeCaptchaUserAllAppId": doDescribeCaptchaUserAllAppId,
     "DescribeCaptchaMiniDataSum": doDescribeCaptchaMiniDataSum,
+    "DescribeCaptchaMiniResult": doDescribeCaptchaMiniResult,
     "UpdateCaptchaAppIdInfo": doUpdateCaptchaAppIdInfo,
     "DescribeCaptchaAppIdInfo": doDescribeCaptchaAppIdInfo,
     "DescribeCaptchaResult": doDescribeCaptchaResult,
