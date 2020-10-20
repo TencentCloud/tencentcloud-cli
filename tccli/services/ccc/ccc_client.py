39d3f10a0254df7eb56863250ead470a5f2f7692
tencentcloudapi <tencentcloudapi@tenent.com>
2020-10-20 10:21:57 +0800

    release 3.0.274.1

--- tccli/services/ccc/ccc_client.py
+++ tccli/services/ccc/ccc_client.py
@@ -13,6 +13,31 @@ from tencentcloud.ccc.v20200210 import ccc_client as ccc_client_v20200210
 from tencentcloud.ccc.v20200210 import models as models_v20200210
 
 
+def doCreateSDKLoginToken(args, parsed_globals):
+    g_param = parse_global_arg(parsed_globals)
+
+    cred = credential.Credential(g_param[OptionsDefine.SecretId], g_param[OptionsDefine.SecretKey])
+    http_profile = HttpProfile(
+        reqTimeout=60 if g_param[OptionsDefine.Timeout] is None else int(g_param[OptionsDefine.Timeout]),
+        reqMethod="POST",
+        endpoint=g_param[OptionsDefine.Endpoint]
+    )
+    profile = ClientProfile(httpProfile=http_profile, signMethod="HmacSHA256")
+    mod = CLIENT_MAP[g_param[OptionsDefine.Version]]
+    client = mod.CccClient(cred, g_param[OptionsDefine.Region], profile)
+    client._sdkVersion += ("_CLI_" + __version__)
+    models = MODELS_MAP[g_param[OptionsDefine.Version]]
+    model = models.CreateSDKLoginTokenRequest()
+    model.from_json_string(json.dumps(args))
+    rsp = client.CreateSDKLoginToken(model)
+    result = rsp.to_json_string()
+    try:
+        jsonobj = json.loads(result)
+    except TypeError as e:
+        jsonobj = json.loads(result.decode('utf-8'))  # python3.3
+    FormatOutput.output("action", jsonobj, g_param[OptionsDefine.Output], g_param[OptionsDefine.Filter])
+
+
 def doDescribeTelCdr(args, parsed_globals):
     g_param = parse_global_arg(parsed_globals)
 
@@ -49,6 +74,7 @@ MODELS_MAP = {
 }
 
 ACTION_MAP = {
+    "CreateSDKLoginToken": doCreateSDKLoginToken,
     "DescribeTelCdr": doDescribeTelCdr,
 
 }
--- tccli/services/sqlserver/sqlserver_client.py
+++ tccli/services/sqlserver/sqlserver_client.py
@@ -1613,6 +1613,31 @@ def doDescribeProductConfig(args, parsed_globals):
     FormatOutput.output("action", jsonobj, g_param[OptionsDefine.Output], g_param[OptionsDefine.Filter])
 
 
+def doRecycleDBInstance(args, parsed_globals):
+    g_param = parse_global_arg(parsed_globals)
+
+    cred = credential.Credential(g_param[OptionsDefine.SecretId], g_param[OptionsDefine.SecretKey])
+    http_profile = HttpProfile(
+        reqTimeout=60 if g_param[OptionsDefine.Timeout] is None else int(g_param[OptionsDefine.Timeout]),
+        reqMethod="POST",
+        endpoint=g_param[OptionsDefine.Endpoint]
+    )
+    profile = ClientProfile(httpProfile=http_profile, signMethod="HmacSHA256")
+    mod = CLIENT_MAP[g_param[OptionsDefine.Version]]
+    client = mod.SqlserverClient(cred, g_param[OptionsDefine.Region], profile)
+    client._sdkVersion += ("_CLI_" + __version__)
+    models = MODELS_MAP[g_param[OptionsDefine.Version]]
+    model = models.RecycleDBInstanceRequest()
+    model.from_json_string(json.dumps(args))
+    rsp = client.RecycleDBInstance(model)
+    result = rsp.to_json_string()
+    try:
+        jsonobj = json.loads(result)
+    except TypeError as e:
+        jsonobj = json.loads(result.decode('utf-8'))  # python3.3
+    FormatOutput.output("action", jsonobj, g_param[OptionsDefine.Output], g_param[OptionsDefine.Filter])
+
+
 def doCreateReadOnlyDBInstances(args, parsed_globals):
     g_param = parse_global_arg(parsed_globals)
 
@@ -1838,6 +1863,7 @@ ACTION_MAP = {
     "ModifyMaintenanceSpan": doModifyMaintenanceSpan,
     "RunMigration": doRunMigration,
     "DescribeProductConfig": doDescribeProductConfig,
+    "RecycleDBInstance": doRecycleDBInstance,
     "CreateReadOnlyDBInstances": doCreateReadOnlyDBInstances,
     "DisassociateSecurityGroups": doDisassociateSecurityGroups,
     "ModifyReadOnlyGroupDetails": doModifyReadOnlyGroupDetails,
