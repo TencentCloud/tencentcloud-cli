6a68930166c80a1f5a6c9001ecf666963ceb16c9
tencentcloudapi <tencentcloudapi@tenent.com>
2020-11-04 08:28:42 +0800

    release 3.0.285.1

--- tccli/services/redis/redis_client.py
+++ tccli/services/redis/redis_client.py
@@ -1063,6 +1063,31 @@ def doDescribeInstanceMonitorBigKeyTypeDist(args, parsed_globals):
     FormatOutput.output("action", jsonobj, g_param[OptionsDefine.Output], g_param[OptionsDefine.Filter])
 
 
+def doDisassociateSecurityGroups(args, parsed_globals):
+    g_param = parse_global_arg(parsed_globals)
+
+    cred = credential.Credential(g_param[OptionsDefine.SecretId], g_param[OptionsDefine.SecretKey])
+    http_profile = HttpProfile(
+        reqTimeout=60 if g_param[OptionsDefine.Timeout] is None else int(g_param[OptionsDefine.Timeout]),
+        reqMethod="POST",
+        endpoint=g_param[OptionsDefine.Endpoint]
+    )
+    profile = ClientProfile(httpProfile=http_profile, signMethod="HmacSHA256")
+    mod = CLIENT_MAP[g_param[OptionsDefine.Version]]
+    client = mod.RedisClient(cred, g_param[OptionsDefine.Region], profile)
+    client._sdkVersion += ("_CLI_" + __version__)
+    models = MODELS_MAP[g_param[OptionsDefine.Version]]
+    model = models.DisassociateSecurityGroupsRequest()
+    model.from_json_string(json.dumps(args))
+    rsp = client.DisassociateSecurityGroups(model)
+    result = rsp.to_json_string()
+    try:
+        jsonobj = json.loads(result)
+    except TypeError as e:
+        jsonobj = json.loads(result.decode('utf-8'))  # python3.3
+    FormatOutput.output("action", jsonobj, g_param[OptionsDefine.Output], g_param[OptionsDefine.Filter])
+
+
 def doDescribeCommonDBInstances(args, parsed_globals):
     g_param = parse_global_arg(parsed_globals)
 
@@ -1388,7 +1413,7 @@ def doDescribeInstanceBackups(args, parsed_globals):
     FormatOutput.output("action", jsonobj, g_param[OptionsDefine.Output], g_param[OptionsDefine.Filter])
 
 
-def doDisassociateSecurityGroups(args, parsed_globals):
+def doModifyConnectionConfig(args, parsed_globals):
     g_param = parse_global_arg(parsed_globals)
 
     cred = credential.Credential(g_param[OptionsDefine.SecretId], g_param[OptionsDefine.SecretKey])
@@ -1402,9 +1427,9 @@ def doDisassociateSecurityGroups(args, parsed_globals):
     client = mod.RedisClient(cred, g_param[OptionsDefine.Region], profile)
     client._sdkVersion += ("_CLI_" + __version__)
     models = MODELS_MAP[g_param[OptionsDefine.Version]]
-    model = models.DisassociateSecurityGroupsRequest()
+    model = models.ModifyConnectionConfigRequest()
     model.from_json_string(json.dumps(args))
-    rsp = client.DisassociateSecurityGroups(model)
+    rsp = client.ModifyConnectionConfig(model)
     result = rsp.to_json_string()
     try:
         jsonobj = json.loads(result)
@@ -1566,6 +1591,7 @@ ACTION_MAP = {
     "DescribeInstanceMonitorBigKey": doDescribeInstanceMonitorBigKey,
     "DescribeInstanceSecurityGroup": doDescribeInstanceSecurityGroup,
     "DescribeInstanceMonitorBigKeyTypeDist": doDescribeInstanceMonitorBigKeyTypeDist,
+    "DisassociateSecurityGroups": doDisassociateSecurityGroups,
     "DescribeCommonDBInstances": doDescribeCommonDBInstances,
     "UpgradeInstance": doUpgradeInstance,
     "DescribeInstanceMonitorHotKey": doDescribeInstanceMonitorHotKey,
@@ -1579,7 +1605,7 @@ ACTION_MAP = {
     "DescribeBackupUrl": doDescribeBackupUrl,
     "DestroyPostpaidInstance": doDestroyPostpaidInstance,
     "DescribeInstanceBackups": doDescribeInstanceBackups,
-    "DisassociateSecurityGroups": doDisassociateSecurityGroups,
+    "ModifyConnectionConfig": doModifyConnectionConfig,
     "RenewInstance": doRenewInstance,
     "ModifyInstance": doModifyInstance,
     "DescribeInstanceMonitorTookDist": doDescribeInstanceMonitorTookDist,
