6c93af16a605c87d4bfb55f5996d43f123855a15
HS-Gray <1978489546@qq.com>
2022-05-23 15:00:24 +0800

    feat: upgrade hint

--- tccli/argparser.py
+++ tccli/argparser.py
@@ -51,6 +51,8 @@ class BaseArgParser(argparse.ArgumentParser):
                 for word in possible:
                     extra.append('  * %s' % word)
                 msg.extend(extra)
+            msg.append("If invalid choice doesn't include the action or product you input, " \
+                       "you can run `pip install --upgrade tccli` to install latest tccli.")
             raise argparse.ArgumentError(action, '\n'.join(msg))
 
     def parse_known_args(self, args=None, namespace=None):
