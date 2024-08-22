a70c3489e5c6c151777bcaf6cf4614861aaa1475
sesky4 <sesky4@gmail.com>
2024-08-22 20:46:11 +0800

    Update add.py

--- tccli/plugins/auth/add.py
+++ tccli/plugins/auth/add.py
@@ -1,5 +1,11 @@
 # coding: utf-8
+import sys
+
 import texts
+import webbrowser
+
+_app_id = 700001249938
+_redirect_url = "https://cli.cloud.tencent.com/oauth"
 
 
 def login_command_entrypoint(args, parsed_globals):
@@ -17,7 +23,12 @@ def login_command_entrypoint(args, parsed_globals):
 
 
 def login():
-    print("login")
+    state = "qwer"
+    entrypoint_url = "https://https://cloud.tencent.com/open/authorize?scope=login&app_id=%s&redirect_url=%s&state=%s" % (
+        _app_id, _redirect_url, state)
+    if not webbrowser.open(entrypoint_url):
+        print(texts.get("login_failed_no_browser"))
+        sys.exit(1)
 
 
 def login_no_browser():
