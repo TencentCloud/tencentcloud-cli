6903f09f76d3089472d833466998853ff247d73a
sesky4 <sesky4@gmail.com>
2025-08-15 16:32:52 +0800

    test: add tests

--- tests/test_cvm.py
+++ tests/test_cvm.py
@@ -1,31 +1,20 @@
-from utils import TestCli
+from utils import shell
+
 
 def test_describe_regions():
-    cmd = 'tccli cvm DescribeRegions'
-    expect = "\"Region\": \"ap-guangzhou\""
-    test_cli = TestCli()
-    test_cli.equal(cmd, expect)
+    assert "\"Region\": \"ap-guangzhou\"" in shell("tccli cvm DescribeRegions")
 
 
 def test_describe_instances():
-    cmd = 'tccli cvm DescribeInstances'
-    expect = "\"TotalCount\":"
-    test_cli = TestCli()
-    test_cli.equal(cmd, expect)
+    assert "\"TotalCount\":" in shell("tccli cvm DescribeInstances")
 
 
 def test_describe_disaster_disaster_recover_group_quota():
-    cmd = 'tccli cvm DescribeDisasterRecoverGroupQuota'
-    expect = "\"CvmInHostGroupQuota\":"
-    test_cli = TestCli()
-    test_cli.equal(cmd, expect)
+    assert "\"CvmInHostGroupQuota\":" in shell("tccli cvm DescribeDisasterRecoverGroupQuota")
 
 
 def test_describe_disaster_recover_groups():
-    cmd = 'tccli cvm DescribeDisasterRecoverGroups'
-    expect = "DisasterRecoverGroupSet"
-    test_cli = TestCli()
-    test_cli.equal(cmd, expect)
+    assert "DisasterRecoverGroupSet" in shell("tccli cvm DescribeDisasterRecoverGroups")
 
 
 def test_describe_hosts():
@@ -33,6 +22,5 @@ def test_describe_hosts():
     cmd += ' --Filters.0.Name zone'
     cmd += ' --Filters.0.Values ap-guangzhou-2'
     cmd += ' --Offset 0 --Limit 20'
-    expect = "\"HostSet\": []"
-    test_cli = TestCli()
-    test_cli.equal(cmd, expect)
+
+    assert "\"HostSet\": []" in shell(cmd)
--- tests/test_helper.py
+++ tests/test_helper.py
@@ -1,30 +1,30 @@
 # -*- coding:utf8 -*-
-from utils import TestCli
+from utils import shell
 
 
 def test_help():
     cmd = 'tccli help'
     expect = 'tccli [options] <service> [options] <action> [options] [options and parameters]'
-    test_cli = TestCli()
-    test_cli.equal(cmd, expect)
+
+    assert expect in shell(cmd)
 
 
 def test_help_detail():
     cmd = 'tccli help --detail'
     expect = 'cvm\n    介绍如何使用API对云服务器进行操作，包括使用并管理实例、镜像、密钥等资源。'
-    test_cli = TestCli()
-    test_cli.equal(cmd, expect)
+
+    assert expect in shell(cmd)
 
 
 def test_cvm_help():
     cmd = 'tccli cvm help'
     expect = '<DescribeRegions>'
-    test_cli = TestCli()
-    test_cli.equal(cmd, expect)
+
+    assert expect in shell(cmd)
 
 
 def test_cvm_help_detail():
     cmd = 'tccli cvm help --detail'
     expect = 'AVAILABLE ACTIONS\n    AllocateHosts'
-    test_cli = TestCli()
-    test_cli.equal(cmd, expect)
+
+    assert expect in shell(cmd)
